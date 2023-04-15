8f4b09ff36357ee94a344ce5ebff36d33177743b
realbits-lab <dev.realbits@gmail.com>
2023-04-15 11:58:37 +0900

    Add exclude directory for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const Merge = require("webpack-merge");
 const terser = require("terser-webpack-plugin");
 
 const baseConfig = {
-  mode: "production",
+  //* TODO: Handle later.
+  // mode: "production",
+  mode: "development",
   devtool: "source-map",
   entry: {
     v3dcore: path.resolve(__dirname, "src", "index"),
@@ -12,10 +14,12 @@ const baseConfig = {
     rules: [
       {
         test: /\.ts$/,
+        exclude: [path.resolve(__dirname, "/lib")],
         use: "ts-loader",
       },
       {
         test: /\.m?js$/,
+        exclude: [path.resolve(__dirname, "/lib")],
         resolve: {
           fullySpecified: false,
         },
@@ -83,68 +87,6 @@ const config = [
   //     path: path.resolve(__dirname, "dist"),
   //   },
   // }),
-];
-
-const config_orig = [
-  // UMD
-  Merge.merge(baseConfig, {
-    output: {
-      library: {
-        name: "v3d-core",
-        type: "umd",
-      },
-      filename: "[name].module.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    externals: [/^@babylonjs.*$/],
-  }),
-  // ES6
-  Merge.merge(baseConfig, {
-    output: {
-      library: {
-        name: "v3d-core",
-        type: "module",
-      },
-      filename: "[name].es6.js",
-      path: path.resolve(__dirname, "dist"),
-      environment: { module: true },
-    },
-    experiments: {
-      outputModule: true,
-    },
-    externalsType: "module",
-    externals: [
-      /^@babylonjs.*$/,
-      /^babylonjs-gltf2interface$/,
-      /^babylon-mtoon-material$/,
-    ],
-  }),
-  // browser global
-  Merge.merge(baseConfig, {
-    output: {
-      library: {
-        name: "v3d-core",
-        type: "window",
-      },
-      filename: "[name].js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    externals: [
-      ({ context, request }, callback) => {
-        if (/^@babylonjs\/core.*$/.test(request)) {
-          return callback(null, "var BABYLON");
-        }
-        // @see https://github.com/BabylonJS/Babylon.js/blob/master/Tools/Config/config.json#L415
-        if (/^@babylonjs\/loaders\/glTF\/2\.0.*$/.test(request)) {
-          return callback(null, "var LOADERS.GLTF2");
-        }
-        if (/^@babylonjs\/loaders.*$/.test(request)) {
-          return callback(null, "var LOADERS");
-        }
-        callback();
-      },
-    ],
-  }),
 ];
 
 module.exports = config;
